122cebb11b365e759aa899904502a753ac2e89b6
rawhite22 <robert.a.white625@gmail.com>
2021-02-09 23:54:25 -0500

    15. working login logout signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,10 +4,10 @@ const { createToken } = require('../utilities/auth.js')
 
 const signup_post = async (req, res) => {
   const { email, password } = req.body
+  console.log(email, password)
   try {
-    const user = new User({ email, password })
+    const user = await User.create({ email, password })
     const token = createToken(user._id)
-    await user.save()
     res.cookie('jwt', token, { httpOnly: true })
     res.status(201).json({ user: user._id })
   } catch (err) {
